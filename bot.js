8be72df2ebebaaf723c6903ee510227a8beb64a3
spamfarm1 <47407388+spamfarm1@users.noreply.github.com>
2019-03-23 16:30:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("559019984937025569")
+setInterval(function() {
+channel.send(`apg apg apg apg apg apg`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
